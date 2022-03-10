01b9bb487e3901a5f67c1fd7cce49a01a55be4ec
Appiva-Tech <brscntyz@gmail.com>
2022-03-10 16:59:09 +0300

    close to final

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ const templateFunction = (category_key,_fname,_lname,_category) => {
     return(`
             <aside class="user-info" id=${category_key}>
                 <div class="profile-section">
-                    <h4><strong class="profile-text">Profile Pic:</strong><img src='https://media.istockphoto.com/vectors/user-icon-people-icon-isolated-on-white-background-vector-vector-id1210939712?k=20&m=1210939712&s=612x612&w=0&h=xJqEPQnMiNofprbLXWdEtJQ75QL79lQ5N76J4JOdTIM='> </h4>
+                    <h4><strong class="profile-text">Profile Pic</strong><img src='https://media.istockphoto.com/vectors/user-icon-people-icon-isolated-on-white-background-vector-vector-id1210939712?k=20&m=1210939712&s=612x612&w=0&h=xJqEPQnMiNofprbLXWdEtJQ75QL79lQ5N76J4JOdTIM='> </h4>
                 </div>   
-                <div class"info-section">
+                <div class="info-section">
                     <h4>First Name: ${_fname} </h4>
                     <h4>Last Name: ${_lname} </h4>
                     <h4>Category: ${_category} </h4>
@@ -30,21 +30,7 @@ const getData = async() => {
     infoData = data
     console.log(data)
     for(let i=0;i<data.length;i++){
-        dataSection.innerHTML += `
-            <aside class="user-info" id=${data[i].category}>
-                <div class="profile-section">
-                    <h4><strong class="profile-text">Profile Pic:</strong><img src='https://media.istockphoto.com/vectors/user-icon-people-icon-isolated-on-white-background-vector-vector-id1210939712?k=20&m=1210939712&s=612x612&w=0&h=xJqEPQnMiNofprbLXWdEtJQ75QL79lQ5N76J4JOdTIM='> </h4>
-                </div>   
-                <div class"info-section">
-                    <h4>First Name: ${data[i].fname} </h4>
-                    <h4>Last Name: ${data[i].lname} </h4>
-                    <h4>Category: ${data[i].category} </h4>
-                </div>
-            </aside>
-
-            
-        `
-
+        dataSection.innerHTML += templateFunction(infoData[i].category,infoData[i].fname,infoData[i].lname,infoData[i].category)
     }
     
 }
